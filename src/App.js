917bed80869f145cbc07a6d7a57edbe1418e3b01
Dev vrat <devvrat709@gmail.com>
2023-01-11 23:35:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Bittoo Aggarwal</h1>
+      <h4>Software Developer, Educator at Geekster</h4>
+      <a href="https://www.linkedin.com/login" >Linkedin</a>&nbsp; 
+      <a href="https://www.github.com" >Github</a>
+      <hr></hr>
+      <h3>Experience</h3>
+      <h4>Software Developer at Amazon</h4>
+      <p>I work with Amazon in transportaion team. occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."</p>
+      <hr></hr>
+      <h3>Educator at Geekster</h3>
+      <p>I work with Geekster in educator team. occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."</p>
+      <hr></hr>
+      <h3>Skills</h3>
+      <ul>
+        <li>Java</li>
+        <li>Javascript</li>
+        <li>Testing</li>
+        <li>API testing</li>
+        <li>System Designing</li>
+      </ul>
+      <hr></hr>
+      <h3>Hobbies</h3>
+      <ol>
+        <li>Table Tennis</li>
+        <li>Reading books</li>
+        <li>Biography</li>
+        <li>Teaching</li>
+      </ol>
+      <hr></hr>
     </div>
   );
 }
